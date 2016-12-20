a5423b83ee0a52daa388b04593c6ea06bc87f37b
Alberto Valverde <albertovalverd@hotmail.com>
2016-12-20 20:02:46 +0100

    Deleting invalid folder

--- Robot-UI/src/pconcept/photo/README.md
+++
@@ -1,4 +0,0 @@
-[gifie](http://eirikb.github.io/gifie)
-=====
-
-Create a gif-selfie using built in HTML5 web cam and [gif.js](http://jnordberg.github.io/gif.js).
--- Robot-UI/src/pconcept/twilio/controllers/router.js
+++
@@ -1,6 +0,0 @@
-// Map routes to controller functions
-module.exports = function(router) {
-  router.get('/error', function(req, resp) {
-    throw new Error('Derp. An error occurred.');
-  });
-};
